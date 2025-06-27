352547df73aa49d368d7a1e2e07ca3568d96bfb7
JustinisaCoder <justinrmedina24@gmail.com>
2025-06-26 21:07:18 -0400

    more methods to shoppingcardao and post put delete methods for shopping cart controller

--- EasyShopStarter/easyshop-api/main/java/org/yearup/controllers/ShoppingCartController.java
+++ EasyShopStarter/easyshop-api/main/java/org/yearup/controllers/ShoppingCartController.java
@@ -1,17 +1,24 @@
-package org.yearup.controllers;
+package org.yearup.controllers;Add commentMore actions
 
 import org.springframework.http.HttpStatus;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 import org.yearup.data.ProductDao;
 import org.yearup.data.ShoppingCartDao;
 import org.yearup.data.UserDao;
+import org.yearup.models.Product;
 import org.yearup.models.ShoppingCart;
+import org.yearup.models.ShoppingCartItem;
 import org.yearup.models.User;
 
 import java.security.Principal;
 
 // convert this class to a REST controller
-// only logged in users should have access to these actions
+// only logged-in users should have access to these actions
+@RestController
+@RequestMapping("cart")
+@CrossOrigin
 public class ShoppingCartController
 {
     // a shopping cart requires
@@ -21,12 +28,14 @@ public class ShoppingCartController
 
 
 
+    @GetMapping
+    @PreAuthorize("permitAll()")
     // each method in this controller requires a Principal object as a parameter
     public ShoppingCart getCart(Principal principal)
     {
         try
         {
-            // get the currently logged in username
+            // get the currently logged-in username
             String userName = principal.getName();
             // find database user by userId
             User user = userDao.getByUserName(userName);
@@ -43,14 +52,59 @@ public class ShoppingCartController
 
     // add a POST method to add a product to the cart - the url should be
     // https://localhost:8080/cart/products/15 (15 is the productId to be added
+    @PostMapping("products/{id}")
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    public ShoppingCartItem addProductToCart(@RequestBody Product product) {
+
+        try
+        {
+            return shoppingCartDao.create(product);
+        }
+        catch(Exception ex)
+        {
+            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "Oops... our bad.");
+        }
+    }
 
 
     // add a PUT method to update an existing product in the cart - the url should be
     // https://localhost:8080/cart/products/15 (15 is the productId to be updated)
     // the BODY should be a ShoppingCartItem - quantity is the only value that will be updated
+    @PutMapping("products/{id}")
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    public void updateShoppingCart(@PathVariable int userId, Product product){
+        // update the shopping cart by id
+        try
+        {
+            shoppingCartDao.update(userId, product);
+        }
+        catch(Exception ex)
+        {
+            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "Oops... our bad.");
+        }
+    }
 
 
     // add a DELETE method to clear all products from the current users cart
     // https://localhost:8080/cart
+    @DeleteMapping
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    public void deleteShoppingCart(@PathVariable int userId)
+    {
+        // delete the shopping cart by user id
+        try
+        {
+            var category = shoppingCartDao.getByUserId(userId);
 
+            if(category == null)
+                throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+
+        }
+        shoppingCartDao.delete(userId);
+    }
+        catch(Exception ex)
+    {
+        throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "Oops... our bad.");
+    }
 }
+    }
--- EasyShopStarter/easyshop-api/main/java/org/yearup/data/ShoppingCartDao.java
+++ EasyShopStarter/easyshop-api/main/java/org/yearup/data/ShoppingCartDao.java
@@ -1,9 +1,15 @@
-package org.yearup.data;
+package org.yearup.data;Add commentMore actions
 
+import org.yearup.models.Category;
+import org.yearup.models.Product;
 import org.yearup.models.ShoppingCart;
+import org.yearup.models.ShoppingCartItem;
 
 public interface ShoppingCartDao
 {
     ShoppingCart getByUserId(int userId);
     // add additional method signatures here
+    ShoppingCartItem create(Product product);
+    void update(int userId, Product product);Add commentMore actions
+    void delete(int userId);
 }
